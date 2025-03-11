1e2a815d48b6b401ab6e2612327af3040f664109
faspix <babkoum05@gmail.com>
2025-03-11 13:48:40 +0100

    refactor: refactor docker files

---
+++ docker/init-db/init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE category_service
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8';
+
+CREATE DATABASE compilation_service
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8';
+
+CREATE DATABASE event_service
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8';
+
+CREATE DATABASE request_service
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8';
+
+CREATE DATABASE statistics_service
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8';
+
+CREATE DATABASE user_service
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8';
