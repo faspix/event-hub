2b3b6e3d2a5b3be4d00e1bd5fa939f526536fe93
faspix <babkoum05@gmail.com>
2025-04-13 01:54:34 +0200

    add: tests for user-service

---
+++ user-service/src/test/java/utility/UserFactory.java
@@ -0,0 +1,35 @@
+package utility;
+
+import com.faspix.dto.RequestUserDTO;
+import com.faspix.dto.ResponseUserDTO;
+import com.faspix.dto.ResponseUserShortDTO;
+
+import java.util.Collections;
+
+public class UserFactory {
+
+    public static ResponseUserDTO makeResponseUser() {
+        return ResponseUserDTO.builder()
+                .userId("1")
+                .email("mail@mail.com")
+                .roles(Collections.singletonList("USER"))
+                .username("username")
+                .build();
+    }
+
+    public static ResponseUserShortDTO makeResponseUserShort() {
+        return ResponseUserShortDTO.builder()
+                .userId("1")
+                .username("username")
+                .build();
+    }
+
+    public static RequestUserDTO makeRequestUser() {
+        return RequestUserDTO.builder()
+                .email("mail@mail.com")
+                .username("username")
+                .password("123123")
+                .build();
+    }
+
+}
