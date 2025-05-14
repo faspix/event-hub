8d2278314be9baff91c1f95546dac18bddeb0629
faspix <babkoum05@gmail.com>
2025-05-14 17:03:16 +0200

    fix: page request maker

--- compilation-service/src/main/java/com/faspix/utility/PageRequestMaker.java
+++ compilation-service/src/main/java/com/faspix/utility/PageRequestMaker.java
@@ -8,15 +8,18 @@ import org.springframework.data.domain.Sort;
 public class PageRequestMaker {
 
     public static Pageable makePageRequest(int from, int size, Sort sort) {
-        if (size > 30) {
+        if (size <= 0 || from < 0)
+            throw new ValidationException("size must be <= 0 and from must be from < 0");
+        if (size > 30)
             throw new ValidationException("Page size cannot be greater then 30");
-        }
+
         return PageRequest.of(from / size, size, sort);
     }
 
     public static Pageable makePageRequest(int from, int size) {
-        return makePageRequest(from / size, size, Sort.unsorted());
+        return makePageRequest(from, size, Sort.unsorted());
     }
 
 }
+
 
