81d558db655f29c03fff51ba4bca69751355e5bd
faspix <babkoum05@gmail.com>
2025-05-23 22:14:50 +0200

    refactor: request-service

--- request-service/src/main/java/com/faspix/service/ConfirmedRequestService.java
+++ request-service/src/main/java/com/faspix/service/ConfirmedRequestService.java
@@ -1,9 +1,23 @@
 package com.faspix.service;
 
+import com.faspix.config.StreamProducersConfiguration;
 import com.faspix.shared.dto.ConfirmedRequestsDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Sinks;
 
-public interface ConfirmedRequestService {
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ConfirmedRequestService {
 
-    void sendConfirmedRequestMsg(ConfirmedRequestsDTO message);
+    private final StreamProducersConfiguration configuration;
+
+    public void sendConfirmedRequestMsg(ConfirmedRequestsDTO message) {
+        configuration.getBus().emitNext(MessageBuilder.withPayload(message).build(), Sinks.EmitFailureHandler.FAIL_FAST);
+        log.debug("Message to sent via Kafka: {}", message);
+    }
 
 }
