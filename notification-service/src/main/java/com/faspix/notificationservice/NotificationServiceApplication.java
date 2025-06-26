3b6f7dc2583a2e3f872971a1929918de1642c658
faspix <babkoum05@gmail.com>
2025-06-26 13:57:27 +0200

    refactor: refactor build.gradle files

--- notification-service/src/main/java/com/faspix/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/faspix/notificationservice/NotificationServiceApplication.java
@@ -2,7 +2,9 @@ package com.faspix.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
