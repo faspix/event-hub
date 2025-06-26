7ffaea247f9fb33ec00a30968dfa45c09d2cda50
faspix <babkoum05@gmail.com>
2025-06-26 13:26:30 +0200

    init notification-service

---
+++ notification-service/src/test/java/com/faspix/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faspix.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
