a0110269160ff045c963401ddd8580e6cbb9fc0e
faspix <babkoum05@gmail.com>
2025-07-03 16:48:10 +0200

    feat: using github maven packages instead of nexus repo (#6)
    * feat: ci pipeline
    
    * fix: cd pipeline
    
    * fix: CI/CD pipelines
    
    * feat: using github maven packages instead of nexus repo
    
    * test pipeline
    
    * test pipeline
    
    * test pipeline
    
    * fix: maven repos
    
    * fix: CI/CD pipeline with github maven packages
    
    * feat: update shared-lib to working with notification service

--- shared-lib/src/main/java/com/faspix/shared/dto/NotificationDTO.java
+++ shared-lib/src/main/java/com/faspix/shared/dto/NotificationDTO.java
@@ -1,16 +1,17 @@
 package com.faspix.shared.dto;
 
+import com.faspix.shared.utility.NotificationType;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class ConfirmedRequestNotificationDTO {
+public class NotificationDTO {
+
+    private final NotificationType type;
 
     private final String userId;
 
     private final String eventName;
-
-    private final Boolean isConfirmed;
 
 }
